9bc6d09afb5d2a3a9200cf168b4fd4d046120bcc
hamzashakoor119 <135787611+hamzashakoor119@users.noreply.github.com>
2024-06-22 04:46:30 +0500

    Add files via upload

---
+++ Tables/tables.js
@@ -0,0 +1,30 @@
+"use strict";
+// ======================= Creating Tables Using Whil Loop ======================= //
+// ============= Table Of "2" : ============= \\
+console.log(`\nThis is the Table of '2':`);
+let a = 1;
+while (a <= 10) {
+    console.log(`2 x ${a} = ${a * 2}`);
+    a++;
+}
+// ============= Table Of "3" : ============= \\
+console.log(`\nThis is the Table of '3':`);
+let b = 1;
+while (b <= 10) {
+    console.log(`3 x ${b} = ${b * 3}`);
+    b++;
+}
+// ============= Table Of "4" : ============= \\
+console.log(`\nThis is the Table of '4':`);
+let c = 1;
+while (c <= 10) {
+    console.log(`4 x ${c} = ${c * 4}`);
+    c++;
+}
+// ============= Table Of "5" : ============= \\
+console.log(`\nThis is the Table of '5':`);
+let d = 1;
+while (d <= 10) {
+    console.log(`5 x ${d} = ${d * 5}`);
+    d++;
+}
